cac868df14b045d322b8d6b42a0a17def6e5a59f
Metin Emre <54211029+metinemredonmez@users.noreply.github.com>
2023-02-05 17:23:58 +0300

    Initial Commit

---
+++ testApp/testAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  testAppApp.swift
+//  testApp
+//
+//  Created by emre on 5.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct testAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
